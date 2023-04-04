9a89d2ea5e3172772f9ba53ee029e3ad17b1ba7a
Hyesung Park <48369800+hpark7@users.noreply.github.com>
2023-04-03 22:56:22 -0400

    reduced to only one option

--- src/GameStart.java
+++ src/GameStart.java
@@ -1,51 +1,33 @@
 import controller.GameController;
 import controller.GameException;
 import view.ConsoleUI;
-import view.GameUI;
 
-import java.util.IllegalFormatException;
 import java.util.Scanner;
 
-import controller.GameController;
-import controller.GameException;
-import view.ConsoleUI;
-import view.GameUI;
-
 /**
- * Class : GameStart.java
- * This class is the launch point for the DB demo
+ * Launches the game.
  */
 public class GameStart {
 
-    public static void main(String[] args) {
-        boolean valid = true;
-        GameController gc = new GameController();
+    public static void main(String[] args) throws GameException {
+        boolean gameStarted = false;
+        GameController gameController = new GameController();
         try {
-            gc.start();
-        } catch (GameException ge) {
-            valid = false;
-            System.out.println(ge.getMessage());
+            gameController.start();
+            gameStarted = true;
+        } catch (GameException e) {
+            System.out.println(e.getMessage());
         }
 
-        if (valid) {
+        if (gameStarted) {
+            System.out.println("Press 1 to start the game.");
             Scanner input = new Scanner(System.in);
-            int choice = 0;
-            do {
-                try {
-                    System.out.println("Please enter 1 to play from the console, 2 to play from the UI.");
-                    choice = input.nextInt();
-                } catch (IllegalFormatException ife) {
-                    System.out.println("You must enter an integer.");
-                }
-                if (!(choice == 1 || choice == 2)) {
-                    System.out.println("You must enter 1 or 2");
-                }
-            } while (!(choice == 1 || choice == 2));
+            int choice = input.nextInt();
             if (choice == 1) {
-                ConsoleUI cui = new ConsoleUI();
-                cui.startGame();
+                ConsoleUI consoleUI = new ConsoleUI();
+                consoleUI.startGame();
             } else {
-                GameUI.launch(GameUI.class);
+                throw new GameException("Invalid input. Please enter 1 to start the game.");
             }
             input.close();
             System.exit(0);
